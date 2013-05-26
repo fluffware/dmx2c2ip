2f38990f9d8ccc765a78d19dd194b4430bfd6eb4
Simon Berg <ksb@fluffware.se>
2013-05-27 01:30:05 +0200

    Configuration file. Device selection.

--- src/main.c
+++ src/main.c
@@ -10,6 +10,42 @@ sigint_handler(gpointer user_data)
   return TRUE;
 }
 
+typedef struct AppContext AppContext;
+struct AppContext
+{
+  char *config_filename;
+  GKeyFile *config_file;
+  gchar *dmx_device;
+  int dmx_speed;
+};
+  
+AppContext app_ctxt  = {
+  NULL,
+  NULL,
+  NULL,
+  250000
+};
+
+static void
+app_init(AppContext *app)
+{
+}
+
+static void
+app_cleanup(AppContext* app)
+{
+  g_free(app->config_filename);
+  if (app->config_filename) g_key_file_unref(app->config_file);
+  g_free(app->dmx_device);
+}
+
+  
+const GOptionEntry app_options[] = {
+  {"config-file", 'c', 0, G_OPTION_ARG_FILENAME,
+   &app_ctxt.config_filename, "Configuration file", "FILE"},
+  {NULL}
+};
+
 int
 main(int argc, char *argv[])
 {
@@ -17,16 +53,44 @@ main(int argc, char *argv[])
   GOptionContext *opt_ctxt;
   GMainLoop *loop;  
   DMXRecv *recv;
+  app_init(&app_ctxt);
   g_type_init();
   opt_ctxt = g_option_context_new (" - map DMX to C2IP");
+  g_option_context_add_main_entries(opt_ctxt, app_options, NULL);
   if (!g_option_context_parse(opt_ctxt, &argc, &argv, &err)) {
     g_printerr("Failed to parse options: %s\n", err->message);
+    app_cleanup(&app_ctxt);
     return EXIT_FAILURE;
   }
   g_option_context_free(opt_ctxt);
-  recv = dmx_recv_new("/dev/ttyAMA0", &err);
+  if (app_ctxt.config_filename) {
+    app_ctxt.config_file = g_key_file_new();
+    if (!g_key_file_load_from_file(app_ctxt.config_file,
+				   app_ctxt.config_filename,
+				   G_KEY_FILE_NONE, &err)) {
+      g_printerr("Failed to read configuration file: %s\n", err->message);
+      app_cleanup(&app_ctxt);
+      return EXIT_FAILURE;
+    }
+  }
+  app_ctxt.dmx_device =
+    g_key_file_get_string(app_ctxt.config_file, "DMXPort", "Device", &err);
+  if (!app_ctxt.dmx_device) {
+    g_printerr("No device: %s\n", err->message);
+    app_cleanup(&app_ctxt);
+    return EXIT_FAILURE;
+  }
+  app_ctxt.dmx_speed =
+    g_key_file_get_integer(app_ctxt.config_file, "DMXPort", "Speed", &err);
+  if (app_ctxt.dmx_speed == 0) {
+    app_ctxt.dmx_speed = 250000;
+    g_clear_error(&err);
+  }
+
+  recv = dmx_recv_new(app_ctxt.dmx_device, &err);
   if (!recv) {
     g_printerr("Failed setup DMX port: %s\n", err->message);
+    app_cleanup(&app_ctxt);
     return EXIT_FAILURE;
   }
   loop = g_main_loop_new(NULL, FALSE);
@@ -36,5 +100,6 @@ main(int argc, char *argv[])
   g_main_loop_unref(loop);
   g_object_unref(recv);
   g_debug("Exiting");
+  app_cleanup(&app_ctxt);
   return EXIT_SUCCESS;
 }
