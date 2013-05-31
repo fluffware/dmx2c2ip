d628678740686fd3ca83e0d73bfe2e4b74dd744c
Simon Berg <ksb@fluffware.se>
2013-05-31 15:23:29 +0200

    More JSON experiments

--- src/http_root/json_util.js
+++ src/http_root/json_util.js
@@ -10,11 +10,26 @@ function createRequest() {
 
 function requestValues(url)
 {
-    jQuery.ajax({url:url, accepts:'application/json',success:handleValueReply});
+    jQuery.getJSON(url, handleValueReply).error(requestError);
 }
 
 
 function handleValueReply(data,status,req)
 {
-    alert("Done"); 
+    $("#result").text(JSON.stringify(data));
+}
+
+function requestError(req,status,error)
+{
+    alert("Request failed ("+status+","+error+")");
+}
+
+function sendValues(url, data)
+{
+    jQuery.post(url, JSON.stringify(data), handleValueReply,"json").error(postError);
+}
+
+function postError(req,status,error)
+{
+    alert("Post request failed ("+status+","+error+")");
 }
